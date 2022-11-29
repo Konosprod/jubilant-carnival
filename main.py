15bb6a59efe8bcf2a9a09a788943214d13385168
Konosprod <konosprod@free.fr>
2022-11-29 17:00:32 +0100

    Convert ugoira to mp4
    Signed-off-by: Konosprod <konosprod@free.fr>

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import shutil
 import pathlib
 import zipfile
 from PIL import Image
+import ffmpeg
 
 ugoira_url = "https://www.pixiv.net/ajax/illust/[ugoiraID]/ugoira_meta?lang=en"
 illust_url = "https://www.pixiv.net/ajax/illust/[illustID]/pages?lang=en"
@@ -15,9 +16,7 @@ headers = {"Referer": "https://www.pixiv.com", "User-Agent": "Mozilla/5.0 (X11; 
 s = requests.Session()
 
 s.headers = headers
-cj = http.cookiejar.MozillaCookieJar("cookies.txt")
-cj.load(ignore_expires=True)
-s.cookies = cj
+
 
 def download_file(url, path):
     with s.get(url, stream=True) as r:
@@ -42,7 +41,37 @@ def get_illust(url, quality="original"):
         path = base_dir.joinpath(url[url.rindex("/")+1:])
         download_file(url, path)
 
-def get_ugoira(url, quality="originalSrc", convert=True):
+def convert_gif(ugoira_id, response, base_dir):
+    imgList = []
+    durationList = []
+    for frame in response["body"]["frames"]:
+        imgList.append(Image.open(base_dir.joinpath(frame["file"])))
+        durationList.append(frame["delay"])
+
+    imgList[0].save(base_dir.joinpath(ugoira_id+".gif"), format="GIF", append_images=imgList[1:], duration=durationList, loop=0, save_all=True)
+
+    return
+
+def convert_mp4(ugoira_id, response, base_dir):
+
+    concat_text = ""
+
+    for frame in response["body"]["frames"]:
+        concat_text += "file \'"+str(base_dir.joinpath(frame["file"]))+"\'\nduration " + str(divmod(frame["delay"] / 1000, 60)[1]) + "\n"
+
+    concat_text += "file \'" + str(base_dir.joinpath(response["body"]["frames"][-1]["file"])) + "\'"
+
+    f = open(base_dir.joinpath("tmp.txt"), "w")
+    f.write(concat_text)
+    f.close()
+
+    ffmpeg.input(base_dir.joinpath("tmp.txt"), format="concat", safe=0).output(str(base_dir.joinpath(ugoira_id+".mp4")), c="copy", **{'vsync':'vfr', 'loglevel': 'quiet'}).run()
+
+    base_dir.joinpath("tmp.txt").unlink(missing_ok=True)
+
+    return
+
+def get_ugoira(url, quality="originalSrc", convertGif=False, convertMp4=False, cleanup=True):
     ugoira_id = url[url.rindex("/")+1:]
     ajax_url = ugoira_url.replace("[ugoiraID]", ugoira_id)
     response = s.get(ajax_url).json()
@@ -59,22 +88,28 @@ def get_ugoira(url, quality="originalSrc", convert=True):
     with zipfile.ZipFile(path, "r") as zip_ref:
         zip_ref.extractall(base_dir)
 
-    if convert:
+    if convertGif:
         print("Building gif file")
-        imgList = []
-        durationList = []
-        for frame in response["body"]["frames"]:
-            imgList.append(Image.open(base_dir.joinpath(frame["file"])))
-            durationList.append(frame["delay"])
+        convert_gif(ugoira_id, response, base_dir)
 
-        imgList[0].save(base_dir.joinpath(ugoira_id+".gif"), format="GIF", append_images=imgList[1:], duration=durationList, loop=0, save_all=True)
+    if convertMp4:
+        print("Building mp4 file")
+        convert_mp4(ugoira_id, response, base_dir)
 
-    path.unlink()
+    if cleanup:
+        files = base_dir.glob("*.jpg")
+        for file in files:
+            file.unlink()
+
+    path.unlink(missing_ok=True)
 
 def main():
+    cj = http.cookiejar.MozillaCookieJar("cookies.txt")
+    cj.load(ignore_expires=True)
+    s.cookies = cj
 
     #get_illust(sys.argv[1])
-    #get_ugoira(sys.argv[1], "src")
+    get_ugoira(sys.argv[1], "src", convertMp4=True, cleanup=True)
     return
 
 
