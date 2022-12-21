721dfae8c5ec568b81dd96ae23a68e0d1dbaae00
Konosprod <konosprod@free.fr>
2022-12-21 12:14:58 +0100

    Download every ugoira/illust from an user
    Signed-off-by: Konosprod <konosprod@free.fr>

--- pixivdl/main.py
+++ pixivdl/main.py
@@ -7,13 +7,20 @@ import pathlib
 import zipfile
 from PIL import Image
 import ffmpeg
+import re
+import time
 
 ugoira_url = "https://www.pixiv.net/ajax/illust/[ugoiraID]/ugoira_meta?lang=en"
 illust_url = "https://www.pixiv.net/ajax/illust/[illustID]/pages?lang=en"
-headers = {"Referer": "https://www.pixiv.com", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"}
+user_url = "https://www.pixiv.net/ajax/user/[userID]/profile/all?lang=en"
+artwork_url = "https://www.pixiv.net/en/artworks/"
 
+regex = re.compile("(?P<id>\d+)")
+
+
+headers = {"Referer": "https://www.pixiv.com",
+           "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/81.0"}
 s = requests.Session()
-
 s.headers = headers
 
 
@@ -25,24 +32,25 @@ def download_file(url, path):
             with open(path, "wb") as output:
                 shutil.copyfileobj(raw, output)
 
-def get_illust(url, quality="original"):
+
+def get_illust(url, quality="original", directory=""):
 
     if quality == None:
         quality = "original"
 
-    illust_id = url[url.rindex("/")+1:]
+    illust_id = regex.search(url).group("id")
     ajax_url = illust_url.replace("[illustID]", illust_id)
     response = s.get(ajax_url).json()
 
-    base_dir = pathlib.Path(illust_id)
+    base_dir = directory / pathlib.Path(illust_id)
     base_dir.mkdir(parents=True, exist_ok=True)
-    
     nb_page = len(response["body"])
     print(f"Downloading {nb_page} pages", flush=True)
     for page in response["body"]:
         url = page["urls"][quality]
         path = base_dir.joinpath(url[url.rindex("/")+1:])
         download_file(url, path)
+
 
 def convert_gif(ugoira_id, response, base_dir):
     imgList = []
@@ -51,39 +59,45 @@ def convert_gif(ugoira_id, response, base_dir):
         imgList.append(Image.open(base_dir.joinpath(frame["file"])))
         durationList.append(frame["delay"])
 
-    imgList[0].save(base_dir.joinpath(ugoira_id+".gif"), format="GIF", append_images=imgList[1:], duration=durationList, loop=0, save_all=True)
+    imgList[0].save(base_dir.joinpath(ugoira_id+".gif"), format="GIF",
+                    append_images=imgList[1:], duration=durationList, loop=0, save_all=True)
 
     return
+
 
 def convert_mp4(ugoira_id, response, base_dir):
 
     concat_text = ""
 
     for frame in response["body"]["frames"]:
-        concat_text += "file \'"+str(base_dir.joinpath(frame["file"]))+"\'\nduration " + str(divmod(frame["delay"] / 1000, 60)[1]) + "\n"
+        concat_text += "file \'"+str(base_dir.joinpath(frame["file"]))+"\'\nduration " + str(
+            divmod(frame["delay"] / 1000, 60)[1]) + "\n"
 
-    concat_text += "file \'" + str(base_dir.joinpath(response["body"]["frames"][-1]["file"])) + "\'"
+    concat_text += "file \'" + \
+        str(base_dir.joinpath(response["body"]["frames"][-1]["file"])) + "\'"
 
     f = open(base_dir.joinpath("tmp.txt"), "w")
     f.write(concat_text)
     f.close()
 
-    ffmpeg.input(base_dir.joinpath("tmp.txt"), format="concat", safe=0).output(str(base_dir.joinpath(ugoira_id+".mp4")), c="copy", **{'vsync':'vfr', 'loglevel': 'quiet'}).run()
+    ffmpeg.input(base_dir.joinpath("tmp.txt"), format="concat", safe=0).output(str(
+        base_dir.joinpath(ugoira_id+".mp4")), c="copy", **{'vsync': 'vfr', 'loglevel': 'quiet'}).run()
 
     base_dir.joinpath("tmp.txt").unlink(missing_ok=True)
 
     return
 
-def get_ugoira(url, quality="originalSrc", convertGif=False, convertMp4=False, cleanup=True):
+
+def get_ugoira(url, quality="originalSrc", convertGif=False, convertMp4=False, cleanup=True, directory=""):
 
     if quality == None:
         quality = "originalSrc"
 
-    ugoira_id = url[url.rindex("/")+1:]
+    ugoira_id = regex.search(url).group("id")
     ajax_url = ugoira_url.replace("[ugoiraID]", ugoira_id)
     response = s.get(ajax_url).json()
 
-    base_dir = pathlib.Path(ugoira_id)
+    base_dir = directory / pathlib.Path(ugoira_id)
     base_dir.mkdir(parents=True, exist_ok=True)
 
     print("Downloading archive", flush=True)
@@ -110,17 +124,56 @@ def get_ugoira(url, quality="originalSrc", convertGif=False, convertMp4=False, c
 
     path.unlink(missing_ok=True)
 
+
+def check_ugoira(url):
+    id = regex.search(url).group("id")
+
+    ajax_url = ugoira_url.replace("[ugoiraID]", id)
+    return not s.get(ajax_url).json()["error"]
+
+
+def get_user(url, video_quality="originalSrc", convertGif=False, convertMp4=False, cleanup=True, image_quality="original"):
+
+    user_id = regex.search(url).group("id")
+    ajax_url = user_url.replace("[userID]", user_id)
+    response = s.get(ajax_url).json()
+
+    base_dir = pathlib.Path(user_id)
+    base_dir.mkdir(parents=True, exist_ok=True)
+    illusts = response["body"]["illusts"]
+
+    if len(illusts) > 0:
+        for illust in illusts:
+            if check_ugoira(illust):
+                get_ugoira(artwork_url + illust, quality=video_quality, convertGif=convertGif,
+                           convertMp4=convertMp4, cleanup=cleanup, directory=str(base_dir))
+            else:
+                continue
+                #get_illust(artwork_url + illust, quality=image_quality, directory=str(base_dir))
+            time.sleep(1)
+
+    return
+
+
 def main():
     parser = argparse.ArgumentParser("pixiv download")
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-i", "--illust", help="Illustration url")
     group.add_argument("-u", "--ugoira", help="Ugoira url")
+    group.add_argument("-b", "--backup", help="User profile url")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--video", help="Convert ugoira to video", action="store_true")
-    group.add_argument("-g", "--gif", help="Convert ugoira to gif", action="store_true")
-    parser.add_argument("-c", "--cookies", help="File containing cookies exported for your browser while beiing logged in pixiv", required=True)
-    parser.add_argument("-k", "--clean-up", help="Clean up useless files when converting ugoiras", action="store_true")
-    parser.add_argument("-q", "--quality", help="Specify the quality of the downloaded file. Can either be [thumb_mini|small|regular|original] for illustration or [src|originalSrc] for ugoiras. Default is max quality", default=None)
+    group.add_argument(
+        "-v", "--video", help="Convert ugoira to video", action="store_true")
+    group.add_argument(
+        "-g", "--gif", help="Convert ugoira to gif", action="store_true")
+    parser.add_argument(
+        "-c", "--cookies", help="File containing cookies exported for your browser while beiing logged in pixiv", required=True)
+    parser.add_argument(
+        "-k", "--clean-up", help="Clean up useless files when converting ugoiras", action="store_true")
+    parser.add_argument("--illust-quality", help="Specify the quality of the downloaded file. Can either be [thumb_mini|small|regular|original]. Default is max quality", choices=[
+                        "thumb_mini", "small", "regular", "original"], default=None)
+    parser.add_argument(
+        "--ugoira-quality", help="Specify the quality of the downloaded file. Can either be [src|originalSrc]. Default is max quality", choices=["src", "originalSrc"], default=None)
 
     args = parser.parse_args()
 
@@ -128,7 +181,7 @@ def main():
         if not pathlib.Path(args.cookies).exists():
             print("You have to specify a valid cookie file !")
             return
-    
+
     cj = http.cookiejar.MozillaCookieJar(args.cookies)
     cj.load(ignore_expires=True)
     s.cookies = cj
@@ -137,7 +190,13 @@ def main():
         get_illust(args.illust, quality=args.quality)
 
     if args.ugoira:
-        get_ugoira(args.ugoira, convertMp4=args.video, cleanup=args.clean_up, convertGif=args.gif, quality=args.quality)
+        get_ugoira(args.ugoira, convertMp4=args.video,
+                   cleanup=args.clean_up, convertGif=args.gif, quality=args.quality)
+
+    if args.backup:
+        get_user(args.backup, convertMp4=args.video, cleanup=args.clean_up, convertGif=args.gif,
+                 video_quality=args.ugoira_quality, image_quality=args.illust_quality)
+
     return
 
 
